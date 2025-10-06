c0f89d8b3d2e21c7dd9789e939cfb5ebb233639e
Gergely Kocsis <kocsisger@example.com>
2025-10-06 08:46:15 +0200

    Observer implemented

--- src/main/java/org/example/MainController.java
+++ src/main/java/org/example/MainController.java
@@ -23,7 +23,10 @@ public class MainController {
 
     @FXML
     void loadData() {
-        nameLabel.setText(model.getStudent().getName());
+        //nameLabel.setText(model.getStudent().getName());
+        //Connects the observer to the observable
+        nameLabel.textProperty().bind(model.getStudent().nameProperty());
+
         creditsLabel.setText("" + model.getStudent().getCredits());
         dateOfBirthLabel.setText(model.getStudent().getDateOfBirth().toString());
 
@@ -32,7 +35,8 @@ public class MainController {
     @FXML
     void changeData() {
         model.getStudent().setName("Tom Smith");
-        nameLabel.setText(model.getStudent().getName());
+        //Not needed since namelabel observes the Student's name
+      //  nameLabel.setText(model.getStudent().getName());
     }
 
     @FXML
